dbe49106bc0d07c48218344ba20989df124ad3e0
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-01-30 20:44:36 +0800

    added HasInviteable trait

---
+++ src/Traits/HasInviteable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CleaniqueCoders\Inviteable\Traits;
+
+/**
+ * HasInviteable Trait
+ */
+trait HasInviteable
+{
+    /**
+     * Get all invitations
+     */
+    public function inviteables()
+    {
+        return $this->morphMany(\CleaniqueCoders\Inviteable\Models\Invite::class, 'inviteable');
+    }
+}
